0381878917832e3d917f6acb09c6a25bdfb32b74
trungnghia112 <trungnghia112@gmail.com>
2022-01-03 11:02:42 +0700

    add utils.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let json_emonsters = require('./data/emonsters.json');
 let json_skills = require('./data/skills.json');
 
 import chooseEnemyMonster from './libs/helper.js';
+import { cube, isNumber } from './libs/utils.js';
+console.log(json_emonsters[0]);
 
-console.log(json_emonsters[0])
-
-const pos = chooseEnemyMonster(1,json_emonsters);
+const pos = chooseEnemyMonster(1, json_emonsters);
 console.log('pos', pos);
+console.log('cube', cube(2));
+console.log('isNumber', isNumber(2));
---
+++ libs/utils.js
@@ -0,0 +1,7 @@
+function cube(x) {
+  return x * x * x;
+}
+function isNumber(value) {
+  return typeof value === 'number';
+}
+export { cube, isNumber };
