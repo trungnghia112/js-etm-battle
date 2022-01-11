99842dd9f40ce759e34da15b404523af7bedcd7b
trungnghia112 <trungnghia112@gmail.com>
2022-01-11 10:46:13 +0700

    update break loop when eM or m with count = 0

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let json_monsters = require('./data/monsters.json');
-let json_emonsters = require('./data/emonsters.json');
-let json_skills = require('./data/skills.json');
+// let json_monsters = require('./data/monsters.json');
+// let json_emonsters = require('./data/emonsters.json');
+// let json_skills = require('./data/skills.json');
+
+const dataJson = require('./data/jsonviewer.json');
+let json_monsters = dataJson.data.monsters;
+let json_emonsters = dataJson.data.eMonsters;
+
 let _ = require('lodash');
 const localDb = require('./libs/db.js');
 let db = new localDb.Database();
@@ -13,6 +18,9 @@ import {
 // console.log(json_emonsters[0]);
 let listMonsters = [];
 let round = 0;
+let monstersCheck = [];
+let monster_count = 0;
+let eMonster_count = 0;
 
 listMonsters = _.orderBy(
   _.compact([...json_monsters, ...json_emonsters]),
@@ -34,6 +42,7 @@ listMonsters = _.orderBy(
     debuff: [],
   };
 });
+// console.log('listMonsters:', listMonsters);
 
 function makeTurn() {
   let listTurns = [];
@@ -85,6 +94,7 @@ function makeTurn() {
         },
       ],
     };
+    // console.log('monster_with_skills:', monster_with_skills);
     const action_monsters = [monster_with_skills];
 
     let currentMonster = listMonsters.find((m) => m._id === monsterAttack._id);
@@ -94,12 +104,15 @@ function makeTurn() {
         currentMonster.fury[0] + 25 > 100 ? 0 : currentMonster.fury[0] + 25,
       ],
     };
+    // console.log('currentMonster:', currentMonster);
+
     const afaction_monsters = bfaction_monsters.map((m) => {
       const eM = action_monsters_skills_targets.find(
         (afm) => afm._id === m._id
       );
       return currentMonster._id === m._id ? currentMonster : eM ? eM : m;
     });
+    // console.log('afaction_monsters:', afaction_monsters);
 
     const turn = {
       data: {
@@ -116,6 +129,7 @@ function makeTurn() {
         },
       },
     };
+    // console.log('turn:', turn);
     // db.create(turn.data);
     listTurns.push(turn);
 
@@ -126,17 +140,23 @@ function makeTurn() {
   });
 
   round = round + 1;
-  const monster = listMonsters.find((v) => v.currenthp > 0);
+  // const monster = listMonsters.find((v) => v.currenthp > 0);
+
+  listMonsters = listMonsters.filter((v) => v.currenthp > 0);
+  monstersCheck = _.groupBy(listMonsters, 'type');
+  // console.log('monstersCheck:', monstersCheck);
+  eMonster_count = monstersCheck.enemy ? monstersCheck.enemy.length : 0;
+  monster_count = monstersCheck.my ? monstersCheck.my.length : 0;
+
   // console.log('round:', round);
-  // console.log('listTurns:', listTurns);
-  if (monster && round < 10) {
+  // console.log('eMonster_count:', eMonster_count);
+  // console.log('monster_count:', monster_count);
+  // console.log('-------------------');
+
+  if (round < 10 && monster_count > 0 && eMonster_count > 0) {
     makeTurn();
   }
 }
 makeTurn();
 
-listMonsters = listMonsters.filter((v) => v.currenthp > 0);
-const monstersCheck = _.groupBy(listMonsters, 'type');
-console.log('monstersCheck:', monstersCheck);
-const win = monstersCheck.my.length > monstersCheck.enemy.length;
-console.log(win ? 'you win' : 'you lose');
+console.log(monster_count > eMonster_count ? 'you win' : 'you lose');
